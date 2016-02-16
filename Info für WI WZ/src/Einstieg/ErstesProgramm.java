483d1b223734905867748365101df3d1b2536f8f
ohein <oliver.hein@t-online.de>
2016-02-16 15:36:07 +0100

    2. Commit

--- Info für WI WZ/src/Einstieg/ErstesProgramm.java
+++ Info für WI WZ/src/Einstieg/ErstesProgramm.java
@@ -1,3 +1,4 @@
+package Einstieg;
 
 public class ErstesProgramm {
 
---
+++ Info für WI WZ/src/Einstieg/Test1.java
@@ -0,0 +1,10 @@
+package Einstieg;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Automatisch generierter Methodenstub
+
+	}
+
+}
