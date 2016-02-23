5f3fbe4f8f0e899784581674d70817e2a98be3c3
ohein <oliver.hein@t-online.de>
2016-02-23 12:02:04 +0100

    gdfgd

--- Info für WI WZ/src/Einstieg/Addition.java
+++ Info für WI WZ/src/Einstieg/Addition.java
@@ -5,16 +5,25 @@ import java.util.Scanner;
 public class Addition {
 
 	public static void main(String[] args) {
+		
+		// Programm zur Addition von zwei Zahlen
+		
 		int a;
 		int b;
 		int c;
+		
 		Scanner eingabe = new Scanner(System.in);
+		
 		System.out.print("1. Summand: ");
 		a=eingabe.nextInt();
+		
 		System.out.print("2. Summand: ");
 		b=eingabe.nextInt();
+		
 		c=a+b;
+		
 		System.out.print("Summe: " + c);
+		
 		eingabe.close();
 	}
 }
